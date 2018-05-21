60b75f6857c481f2619b1028890f31f11076ce2b
Totalfreak <dgkartin@live.dk>
2018-05-21 18:18:24 +0200

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,30 @@
+const electron = require('electron');
+const {shell, app, BrowserWindow} = electron;
+const HOMEPAGE = 'https://v2.overleaf.com/project';
+
+let mainWindow;
+
+app.on('ready', () => {
+  window = new BrowserWindow({
+    width: 1024,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+  window.setMenuBarVisibility(false);
+  window.loadURL(HOMEPAGE);
+
+  //Handling external urls
+  /*window.webContents.on('will-navigate', (ev, url) => {
+    parts = url.split('/');
+    if(parts[0] + '//' + parts[2] != HOMEPAGE) {
+      ev.preventDefault();
+      shell.openExternal(url);
+    };
+  });*/
+
+  window.on('closed', () => {
+    window = null;
+  });
+});
